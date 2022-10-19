0e8f384e8e390a6bfd76597e147ba5cb6027d7cc
harshitkk11 <114001877+harshitkk11@users.noreply.github.com>
2022-10-19 21:34:38 +0530

    Done

---
+++ Twosum.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void twoSum(vector<int> &nums, int target)
+    {
+        bool flag = false;
+        for(int i=0; i<nums.size(); i++){
+            for (int j = 1; j < nums.size(); j++)
+            {
+                if((nums[i]+nums[j]) == target){
+                    if(!flag){
+                        cout<< "["<<i<<","<<j<<"]"<<endl;
+                        flag = true;
+                    }
+
+                }
+
+            }
+            
+        }
+}
+
+
+int main(){
+
+    int n;
+    int target;
+
+    cin>>n>>target;
+
+    vector<int>nums;
+
+    for(int i=0; i<n; i++){
+        int num;
+        cin>>num;
+
+        nums.push_back(num);
+    }
+
+    twoSum(nums, target);
+
+    return 0;
+}
