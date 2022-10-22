156bcb6a91b3dc6fe7cea4a1c118b552e1f0f005
harshitkk11 <114001877+harshitkk11@users.noreply.github.com>
2022-10-22 23:56:37 +0530

    Solution

--- Twosum.cpp
+++ Twosum.cpp
@@ -6,7 +6,7 @@ void twoSum(vector<int> &nums, int target)
     {
         bool flag = false;
         for(int i=0; i<nums.size(); i++){
-            for (int j = 1; j < nums.size(); j++)
+            for (int j = i+1; j < nums.size(); j++)
             {
                 if((nums[i]+nums[j]) == target){
                     if(!flag){
